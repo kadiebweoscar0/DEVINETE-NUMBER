7021d207440e2b742943a74b532e3d3e77213cd5
oscar kadiebwe <kadiebweoscar0@gmail.com>
2023-10-13 17:34:06 +0100

    select all a button

---
+++ devinet.js
@@ -0,0 +1,4 @@
+//select all button
+const messageElement = document.querySelector(".message");
+const inputElement = document.getElementById("input");
+const btnDevinete = document.getElementById("Devinete");
