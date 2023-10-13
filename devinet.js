794af66c729d0d097aa5fd12fb8f20e260c26962
oscar kadiebwe <kadiebweoscar0@gmail.com>
2023-10-13 20:22:36 +0100

    add a logique for operation application the DEVINETE-NUMBER

--- devinet.js
+++ devinet.js
@@ -3,5 +3,27 @@ const messageElement = document.querySelector(".message");
 const inputElement = document.getElementById("input");
 const btnDevinete = document.getElementById("Devinete");
 
-let random = Math.floor(Math.random() * 99)
+let random = Math.floor(Math.random() * 99);
 
+btnDevinete.addEventListener('click', ()=>{
+    if (inputElement.value > random) {
+        messageElement.style.backgroundColor = "rgba(197, 30, 64, 0.664)";
+        messageElement.style.display = 'block';
+        messageElement.innerHTML = 'votre chiffre est trop grand';
+        inputElement.value = inputElement.value
+        messageElement.setAttribute("class","message");
+    }
+    else if(inputElement.value < random){
+        messageElement.style.backgroundColor = "rgba(197, 30, 64, 0.664)";
+        messageElement.style.display = 'block';
+        messageElement.innerHTML = 'votre chiffre est trop petit';
+        inputElement.value = inputElement.value;
+        messageElement.setAttribute("class","message");
+    }else{
+        messageElement.style.backgroundColor = "rgba(30, 160, 30, 0.575)";
+        messageElement.style.display = 'block';
+        messageElement.innerHTML = 'Bravo vous avez Deviner';
+        inputElement.value = inputElement.value;
+        messageElement.setAttribute("class","message");
+    }
+})
